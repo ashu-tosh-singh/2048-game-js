9d4efc2cbf4ed029e9049d7ea43543c947027647
ashu-tosh-singh <ashutosh.singh2780@gmail.com>
2023-05-21 02:40:40 +0530

    updates components

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Tile from "./Tile";
 import Cell from "./Cell";
+import { Board } from "../helper";
 
-const Board = () => {
-  return <div>Board</div>;
+const BoardView = () => {
+  const [board, setBoard] = useState(new Board());
+
+  const cells = board.cells.map((row, rowIndex) => {
+    return (
+      <div>
+        {row.map((col, colIndex) => {
+          return <Cell key={rowIndex * board.size + colIndex} />;
+        })}
+      </div>
+    );
+  });
+
+  const tiles = board.tiles
+    .filter((tile) => tile.value !== 0)
+    .map((tile, index) => {
+      return <Tile tile={tile} key={index} />;
+    });
+
+  return (
+    <div>
+      <div className="board">
+        {cells}
+        {tiles}
+      </div>
+    </div>
+  );
 };
 
-export default Board;
+export default BoardView;
