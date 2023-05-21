6b82c9078f6440dda63cd9181fff351e3c2a764c
ashu-tosh-singh <ashutosh.singh2780@gmail.com>
2023-05-22 01:45:07 +0530

    updated header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h5>2048 Game</h5>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./main.scss";
-import "./styles.scss";
 import BoardView from "./components/Board";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import "./styles/footer.scss";
+import "./styles/header.scss";
+import "./main.scss";
+import "./styles.scss";
 
 const App = () => {
   return (
     <>
+      <Header />
       <BoardView />
       <Footer />
     </>
